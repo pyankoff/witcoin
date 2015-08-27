fbf847482eded4e99876201477e3f39489a0d12b
pyankoff <pyankoff@gmail.com>
2015-08-27 17:48:20 +0700

    working transaction

---
+++ client/app.browserify.js
@@ -0,0 +1,3 @@
+bitChannel = require('bitcore-channel');
+bitExplorers = require('bitcore-explorers');
+bitcore = require('bitcore');
--- client/main.js
+++ client/main.js
@@ -1,9 +1,12 @@
 Template.customer.onRendered(function() {
-  var Consumer = bitChannel.Consumer;
-  
-  
 
 })
+
+Template.provider.onRendered(function() {
+  var providerKey = new bitcore.PrivateKey(bitcore.Networks.testnet);
+
+  console.log('provider key: ' + providerKey.toString());
+});
 
 Template.messageList.helpers({
   messages: function () {
@@ -27,10 +30,42 @@ Template.newMessage.events({
   "submit .new-message": function (event) {
     event.preventDefault();
 
-    var name = event.target.name.value;
+    var name = "makaka";
     var text = event.target.message.value;
 
-    Meteor.call("sendMessage", name, text);
+    var insight = new bitExplorers.Insight('testnet');
+
+    var providerAddress = 'mrS6NTGrcW8QT1g3caYupyQtxvk9WM2zim'
+
+    var customerAddress = 'mkAHsyhDGe9yA271RQ9rQSJVYbFtwx5HiE'
+    var customerPrivate = 'a96511bd65dc4fbb0409bf3e51ac8e18853e05c6e133b1b1d78e1d34a995d1a7'
+
+    insight.getUnspentUtxos(customerAddress, function(err, utxos) {
+      if (!err) {
+        console.log("utxos: ", utxos);
+
+        var transaction = new bitcore.Transaction()
+            .from(utxos[0])
+            .to(providerAddress, 100000)
+            .change(customerAddress)
+            .fee(1000)
+            .sign(customerPrivate);
+
+        insight.broadcast(transaction, function(err, txId) {
+          if (err) { 
+            console.log(err) 
+          } else { 
+            console.log(txId)
+            Meteor.call("sendMessage", name, text, txId);
+          } 
+        });
+
+      } else {
+        console.log(err);
+      } 
+    });
+
+    
 
     event.target.message.value = "";
   }
