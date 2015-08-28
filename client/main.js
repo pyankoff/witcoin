a0c7df8207434119018683c558f3109c2ed057b8
pyankoff <pyankoff@gmail.com>
2015-08-28 10:45:55 +0700

    add create new channel

--- client/main.js
+++ client/main.js
@@ -1,22 +1,43 @@
-Template.customer.onRendered(function() {
+Template.createChannel.events({
+  "submit .new-channel": function(event) {
+    event.preventDefault();
 
-})
+    var channel = {
+          providerId: Random.id(),
+          providerAddress: event.target.address.value,
+          providerName: event.target.name.value
+    }
 
-Template.provider.onRendered(function() {
-  var providerKey = new bitcore.PrivateKey(bitcore.Networks.testnet);
+    Session.set("providerId", channel.providerId);
 
-  console.log('provider key: ' + providerKey.toString());
+    Meteor.call("createChannel", channel, 
+      function(error, result) {
+        if (!error) {
+          Router.go("channel", {_id: result._id, hii: "asdf"});
+        }
+      });
+  }
+});
+
+Template.channel.helpers({
+  channelName: function() {
+    return this.providerName;
+  },
+  isProvider: function() {
+    return this.providerId == Session.get('providerId');
+  }
 });
 
 Template.messageList.helpers({
   messages: function () {
-    var query = Messages.find({});
+    console.log(this._id);
+    var query = Messages.find({channelId: this._id});
 
     var init = true;
 
     query.observeChanges({
       added: function (id, message) {
-          if ( ! init) 
+          if (!init) 
             console.log(id);
       }
     });
@@ -26,26 +47,45 @@ Template.messageList.helpers({
   }
 });
 
-Template.newMessage.events({
+Template.newProviderMessage.events({
   "submit .new-message": function (event) {
     event.preventDefault();
 
-    var name = "makaka";
-    var text = event.target.message.value;
+    var message = {
+      channelId: this._id,
+      text: event.target.message.value,
+      sender: this.providerName
+    }
+
+    Meteor.call("sendMessage", message);
+
+    event.target.message.value = "";
+  }
+});
+
+Template.newClientMessage.events({
+  "submit .new-message": function (event) {
+    event.preventDefault();
+
+    var message = {
+      channelId: this._id,
+      text: event.target.message.value,
+      sender: "makaka"
+    }
 
     var insight = new bitExplorers.Insight('testnet');
 
-    var providerAddress = 'mrS6NTGrcW8QT1g3caYupyQtxvk9WM2zim'
+    var providerAddress = "mrS6NTGrcW8QT1g3caYupyQtxvk9WM2zim";//this.providerAddress;
 
     var customerAddress = 'mkAHsyhDGe9yA271RQ9rQSJVYbFtwx5HiE'
     var customerPrivate = 'a96511bd65dc4fbb0409bf3e51ac8e18853e05c6e133b1b1d78e1d34a995d1a7'
 
     insight.getUnspentUtxos(customerAddress, function(err, utxos) {
       if (!err) {
-        console.log("utxos: ", utxos);
+        // console.log("utxos: ", utxos);
 
         var transaction = new bitcore.Transaction()
-            .from(utxos[0])
+            .from(utxos)
             .to(providerAddress, 100000)
             .change(customerAddress)
             .fee(1000)
@@ -55,8 +95,8 @@ Template.newMessage.events({
           if (err) { 
             console.log(err) 
           } else { 
-            console.log(txId)
-            Meteor.call("sendMessage", name, text, txId);
+            message.txId = txId;
+            Meteor.call("sendMessage", message);
           } 
         });
 
@@ -64,8 +104,6 @@ Template.newMessage.events({
         console.log(err);
       } 
     });
-
-    
 
     event.target.message.value = "";
   }
